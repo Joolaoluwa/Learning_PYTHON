bb8f73adaa360f9b9a27ea43b935fe498b971dc5
olusola joshua <joolaoluwa2002@gmail.com>
2024-04-30 20:37:37 +0100

    Learning something new

---
+++ Chapter 1-crash course/Exercise-1.py
@@ -0,0 +1,9 @@
+# Exercise 1
+# message = "My name is Joshua"
+# print(message);
+
+# Exercise 2
+message = "I don't like work"
+print(message)
+message = "I am learning discipline"
+print(message)
---
+++ Chapter 1-crash course/hello.py
@@ -0,0 +1,6 @@
+# message = "Hello World"
+# print(message)
+name = "Olusola Joshua"
+message = "i am a boy" + ", my Name is " + name
+# print(message.title())
+print(message)
