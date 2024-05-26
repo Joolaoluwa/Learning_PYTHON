33fe96ec75352964e3ff81506f97366601832fb3
olusola joshua <joolaoluwa2002@gmail.com>
2024-05-26 14:48:45 +0100

    Floating point numbers in python and operations performed

--- Chapter 1-crash course/numbers.py
+++ Chapter 1-crash course/numbers.py
@@ -1,9 +1,21 @@
 #integers
-number1 = 10
-number2 = 4
-multiply = number1 * number2
-addition = number1 + number2
-divide = number1 / number2
-square = number1 ** number2
-multiple_op = multiply + addition
-print(multiply, "\n", addition, "\n", divide, "\n", multiple_op, "\n", square)
+# I made an observation when dividing two integers in python,
+# The division gave a floating point variable, but in other languages like C++,
+# the floating point is truncated
+# number1 = 10
+# number2 = 4
+# multiply = number1 * number2 # multiplication operation
+# addition = number1 + number2 # addition ooperation
+# divide = number1 / number2 # division operation
+# square = number1 ** number2 # square operation
+# multiple_op = multiply + addition
+# print(multiply, "\n", addition, "\n", divide, "\n", multiple_op, "\n", square)
+
+#Floating point
+#Any number preceded by a point is known as a floating point number
+fn1 = 42.54  # fn1 for floating point 1
+fn2 = 32.54  # fn2 for floating point 2
+multiply = fn1 * fn2 # multiplication operation
+addition = fn1 + fn2 # addition ooperation
+divide = fn1 / fn2 # division operation
+print(multiply, "\n", addition, "\n", divide, "\n")
