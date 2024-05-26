bb57c323f00dd107531d6ff0b34cd2b40396126c
olusola joshua <joolaoluwa2002@gmail.com>
2024-05-26 15:04:11 +0100

    Exercise-3 in learning python

---
+++ Chapter 1-crash course/Exercise-3.py
@@ -0,0 +1,9 @@
+#Exercise 3
+print(2 * 4) #multiplication
+print(10 - 2) #subtraction
+print(5 + 3) #addition
+print(80 / 10) #division
+
+#Exercise 4
+myFav = 5
+print ("My favourite number is " + str(myFav) + " and I think it's my lucky number")
