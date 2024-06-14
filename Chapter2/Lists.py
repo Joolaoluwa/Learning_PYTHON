d7df112149e36e40b5c25fc54c62345838d55223
olusola joshua <joolaoluwa2002@gmail.com>
2024-06-14 23:14:59 +0100

    New changes added

--- Chapter2/Lists.py
+++ Chapter2/Lists.py
@@ -1,3 +1,14 @@
 # Example 1, containing a list in python
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+
+#Example 2, Accessing a list, lets access the first element in the list
+print(bicycles[0])
+print(bicycles[0].title()) #String manipulation on an item in the list
+
+#Example 3, Accessing the last element on the list
+print(bicycles[-1])
+
+#Example 4, using individual values from a list
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
