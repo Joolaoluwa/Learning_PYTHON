cd691c5c2ebd5094d21a1d43c03c7f17c89a6431
olusola joshua <joolaoluwa2002@gmail.com>
2024-06-14 23:24:52 +0100

    Using individual values

--- Chapter2/Exercise-4.py
+++ Chapter2/Exercise-4.py
@@ -4,3 +4,9 @@ print(Names[0])
 print(Names[1])
 print(Names[2])
 print(Names[2])
+
+#Greetings
+greet1 = 'Hi, ' + Names[0] 
+greet2 = 'Hi, ' + Names[1] 
+greet3 = 'Hi, ' + Names[2] 
+greet4 = 'Hi, ' + Names[3] 
