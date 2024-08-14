4ded9d3cd461e6dfa85f4336701e62a9edbe4ecd
olusola joshua <joolaoluwa2002@gmail.com>
2024-08-14 14:15:15 +0100

    New solution added

--- Chapter2/Exercise-4.py
+++ Chapter2/Exercise-4.py
@@ -1,12 +1,17 @@
 #Names
-Names = ['Dara', 'Ftune', 'Big Yufs', 'Lateef']
-print(Names[0])
-print(Names[1])
-print(Names[2])
-print(Names[2])
+names = ['Dara', 'Ftune', 'Big Yufs', 'Lateef']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[2])
 
 #Greetings
-greet1 = 'Hi, ' + Names[0] 
-greet2 = 'Hi, ' + Names[1] 
-greet3 = 'Hi, ' + Names[2] 
-greet4 = 'Hi, ' + Names[3] 
+greet1 = 'Hi, ' + names[0]
+greet2 = 'Hi, ' + names[1] 
+greet3 = 'Hi, ' + names[2] 
+greet4 = 'Hi, ' + names[3] 
+
+#Transportation
+cars = ['Lexus', 'Audi',]
+goal = "I want a " + cars[0] + " Rx350 and an " + cars[1] + " Suv"
+print(goal)
