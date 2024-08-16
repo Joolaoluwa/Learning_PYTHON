750596663ef4bdb6dd18d4d3a19052219f2c6363
olusola joshua <joolaoluwa2002@gmail.com>
2024-08-16 13:07:04 +0100

    Exercise completed, new things learnt

--- Chapter2/Exercise_5.py
+++ Chapter2/Exercise_5.py
@@ -22,3 +22,19 @@ guests.append("YUY investement")
 print(guests)
 
 #Shrinking Guests
+print("So sorry to be the bearer of bad news, but the bigger table wont be available to accomodate y'all")
+guest1 = guests.pop()
+print("Sorry, you're no more invited, " + guest1)
+guest2 = guests.pop()
+print("Sorry, you're no more invited, " + guest2)
+guest3 = guests.pop()
+print("Sorry, you're no more invited, " + guest3)
+guest4 = guests.pop()
+print("Sorry, you're no more invited, " + guest4)
+guest5 = guests.pop()
+print("Sorry, you're no more invited, " + guest5)
+print(guests)
+print("Congratulations " + guests[0] + " and " + guests[1] + " you've been invited to the greatest dinner ever")
+del guests[1]
+del guests[0]
+print(guests)
